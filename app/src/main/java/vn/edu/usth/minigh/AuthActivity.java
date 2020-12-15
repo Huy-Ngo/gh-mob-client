fff786ae465353e6697b5d35a8367b5da5c3110e
Ngô Xuân Minh <xminh.ngo.00@gmail.com>
2020-12-15 23:26:19 +0700

    Remove unnecessary override Also correct background color

--- app/src/main/java/vn/edu/usth/minigh/AuthActivity.java
+++ app/src/main/java/vn/edu/usth/minigh/AuthActivity.java
@@ -6,9 +6,7 @@ import android.os.Bundle;
 
 public class AuthActivity extends AppCompatActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_auth);
+    public AuthActivity() {
+        super(R.layout.activity_auth);
     }
 }
