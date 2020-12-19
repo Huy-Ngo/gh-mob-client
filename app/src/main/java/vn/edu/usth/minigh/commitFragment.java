0e977781454b42bbf952a17e1e5517b7fb03951e
Minh Long <migloh_@users.noreply.github.com>
2020-12-19 18:12:09 +0700

    New commit fragment

---
+++ app/src/main/java/vn/edu/usth/minigh/commitFragment.java
@@ -0,0 +1,35 @@
+package vn.edu.usth.minigh;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class commitFragment extends Fragment {
+
+    public commitFragment() {
+        super(R.layout.fragment_commit);
+    }
+
+    public static commitFragment newInstance() {
+        commitFragment fragment = new commitFragment();
+        Bundle args = new Bundle();
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_commit, container, false);
+    }
+}
