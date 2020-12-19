3ad7f27e066baf7793aded9c201ff5a42dd6eefc
Huy-Ngo <duchuy29092000@gmail.com>
2020-12-19 11:52:51 +0700

    [WIP] Add fragments for previewing repo

--- app/src/main/java/vn/edu/usth/minigh/ProfileViewPagerAdapter.java
+++ app/src/main/java/vn/edu/usth/minigh/ProfileViewPagerAdapter.java
@@ -1,3 +1,20 @@
+/* ViewPager adapter for profile activity
+ * Copyright (C) 2020  Ngô Ngọc Đức Huy
+ * This file is part of MiniGH.
+ *
+ * MiniGH is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * MiniGH is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with MiniGH.  If not, see <https://www.gnu.org/licenses/>.
+ **/
 package vn.edu.usth.minigh;
 
 import android.content.Context;
@@ -32,6 +49,8 @@ public class ProfileViewPagerAdapter extends FragmentPagerAdapter {
     public CharSequence getPageTitle(int position) {
         switch (position) {
             case 0: return "Pinned";
+            case 1: return "Repositories";
+            case 2: return "Contribution";
             default: return super.getPageTitle(position);
         }
     }
