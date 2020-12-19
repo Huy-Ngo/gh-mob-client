43ab422dc40ed302e4eb9f13a4ed32ae7471f56a
Huy-Ngo <duchuy29092000@gmail.com>
2020-12-19 10:28:54 +0700

    Create layout for user profile

--- app/src/main/java/vn/edu/usth/minigh/ProfileActivity.java
+++ app/src/main/java/vn/edu/usth/minigh/ProfileActivity.java
@@ -1,4 +1,22 @@
+/* Activity for user profile
+ * Copyright (C) 2020  Ngô Ngọc Đức Huy
+ * This file is part of MiniGH.
+ *
+ * MiniGH is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * MiniGH is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with MiniGH.  If not, see <https://www.gnu.org/licenses/>.
+**/
 package vn.edu.usth.minigh;
+
 
 import android.app.Activity;
 import android.app.AlertDialog;
@@ -30,7 +48,7 @@ public class ProfileActivity extends AppCompatActivity {
         following.setText((Html.fromHtml("<b>"+5+"</b> Following")));
 
         txt_toolbar = (TextView) findViewById(R.id.main_text_bar);
-        txt_toolbar.setText("Profile");
+        txt_toolbar.setText(this.getApplicationContext().getText(R.string.profile));
     }
 
     public void ClickMenu(View view){
