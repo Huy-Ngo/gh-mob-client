6e218ef8e61a119725c8506fb3cfdaaf8f627bac
Trinh Thao Phuong <thaophuong.trinh.9@gmail.com>
2020-12-23 11:29:16 +0700

    Add things dynamic (still not working)

--- app/src/main/java/vn/edu/usth/minigh/IssueActivity.java
+++ app/src/main/java/vn/edu/usth/minigh/IssueActivity.java
@@ -49,9 +49,13 @@ public class IssueActivity extends BaseActivity {
             FragmentManager fm = getSupportFragmentManager();
             frag = fm.findFragmentById(R.id.issuesFragment);
             FragmentTransaction ft = fm.beginTransaction();
-            frag = new IssuesOpenFragment();
+            frag = new IssueFragment();
+            Bundle bundle = new Bundle();
+            bundle.putString("IssueStatus",  txt);
+            frag.setArguments(bundle);
             ft.replace(R.id.issuesFragment, frag);
             ft.commit();
+
         }
         if(txt == "Close"){
             FragmentManager fm = getSupportFragmentManager();
@@ -60,6 +64,48 @@ public class IssueActivity extends BaseActivity {
             frag = new IssuesClosedFragment();
             ft.replace(R.id.issuesFragment, frag);
             ft.commit();
+//            FrameLayout fl = (FrameLayout)findViewById(R.id.issuesFragment);
+//            for (int i = 0; i< 3; i++) {
+//                LinearLayout ll = new LinearLayout(this);
+//                LinearLayout.LayoutParams paramll = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+//                paramll.setMargins(0, 10, 0, 10);
+//                ll.setLayoutParams(paramll);
+//                ll.setOrientation(LinearLayout.HORIZONTAL);
+//                ll.setBackground(getResources().getDrawable(R.drawable.styling_commit));
+//                ll.setHorizontalGravity(View.TEXT_ALIGNMENT_CENTER);
+//
+//
+//                ImageView iv = new ImageView(this);
+//                LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+//                params.setMargins(12, 12, 12, 12);
+//                iv.setLayoutParams(params);
+//                iv.setScaleType(ImageView.ScaleType.FIT_START);
+//                iv.setPadding(12, 12, 12, 12);
+//                iv.setBackground(getResources().getDrawable(R.drawable.done));
+//                ll.addView(iv);
+//
+//                LinearLayout llinner = new LinearLayout(this);
+//                LinearLayout.LayoutParams paramllInner = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+//                paramllInner.setMargins(10, 10, 10, 10);
+//                llinner.setLayoutParams(paramllInner);
+//                llinner.setOrientation(LinearLayout.VERTICAL);
+//                ll.addView(llinner);
+//
+//                TextView reponame = new TextView(this);
+//                LinearLayout.LayoutParams txtparam = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+//                reponame.setTextSize(18);
+//                reponame.setLayoutParams(txtparam);
+//                reponame.setText("GHname/GHREPOname #8"+i);
+//                llinner.addView(reponame);
+//
+//                TextView issues = new TextView(this);
+//                issues.setTextSize(18);
+//                issues.setLayoutParams(txtparam);
+//                issues.setText("Ex PR #8");
+//                issues.setTextColor(getResources().getColor(R.color.secondaryTextColor));
+//                llinner.addView(issues);
+//                fl.addView(ll);
+//            }
         }
     }
 }
--- app/src/main/java/vn/edu/usth/minigh/IssueFragment.java
+++ app/src/main/java/vn/edu/usth/minigh/IssueFragment.java
@@ -4,6 +4,9 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.FrameLayout;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import androidx.fragment.app.Fragment;
@@ -59,11 +62,49 @@ public class IssueFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        String strText = getArguments().getString("issueStatus");
-        TextView txt_check = (TextView) getView().findViewById(R.id.textCheck);
-        txt_check.setText(strText);
-        txt_check.setTextColor(getResources().getColor(R.color.primaryColor));
+        View view =inflater.inflate(R.layout.fragment_issue, container, false);
+        FrameLayout fl = (FrameLayout)view.findViewById(R.id.issuesFragment);
+        for (int i = 0; i< 5; i++) {
+            LinearLayout ll = new LinearLayout(this.getActivity());
+            LinearLayout.LayoutParams paramll = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+            paramll.setMargins(0, 10, 0, 10);
+            ll.setLayoutParams(paramll);
+            ll.setOrientation(LinearLayout.HORIZONTAL);
+            ll.setPadding(10,10,10,10);
+            ll.setBackground(getResources().getDrawable(R.drawable.styling_commit));
+            ll.setHorizontalGravity(View.TEXT_ALIGNMENT_CENTER);
 
-        return inflater.inflate(R.layout.fragment_issue, container, false);
+            ImageView iv = new ImageView(this.getContext());
+            LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+            params.setMargins(12, 12, 12, 12);
+            iv.setLayoutParams(params);
+            iv.setScaleType(ImageView.ScaleType.FIT_START);
+            iv.setPadding(12, 12, 12, 12);
+            iv.setBackground(getResources().getDrawable(R.drawable.pricon));
+            ll.addView(iv);
+
+            LinearLayout llinner = new LinearLayout(this.getContext());
+            LinearLayout.LayoutParams paramllInner = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+            paramllInner.setMargins(10, 10, 10, 10);
+            llinner.setLayoutParams(paramllInner);
+            llinner.setOrientation(LinearLayout.VERTICAL);
+            ll.addView(llinner);
+
+            TextView reponame = new TextView(this.getContext());
+            LinearLayout.LayoutParams txtparam = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+            reponame.setTextSize(18);
+            reponame.setLayoutParams(txtparam);
+            reponame.setText("GHname/GHREPOname #8"+i);
+            llinner.addView(reponame);
+
+            TextView issues = new TextView(this.getContext());
+            issues.setTextSize(18);
+            issues.setLayoutParams(txtparam);
+            issues.setText("Ex PR #8");
+            issues.setTextColor(getResources().getColor(R.color.secondaryTextColor));
+            llinner.addView(issues);
+            fl.addView(ll);
+        }
+        return view;
     }
 }
